4fd2ed13d8195f317610455603a17fe7c85998aa
puneet56 <puneetdhiman56@gmail.com>
2021-11-08 23:49:24 +0530

    Added:Page-Login, Comp-OTP

---
+++ src/components/Otp.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import tw from 'tailwind-styled-components';
+
+const Wrapper = tw.form`md:min-h-[80vh] min-h-[60vh] w-full max-w-[90vw] bg-gray-100 bg-opacity-40 rounded-3xl shadow-2xl flex flex-col items-center justify-center py-8 space-y-9
+`;
+const SubHeading = tw.h2`text-3xl font-medium md:text-5xl md:font-normal text-center text-white
+`;
+const Input = tw.input`ml-[20px] max-w-[190px] h-12 tracking-[30px] placeholder-shown:tracking-normal focus:outline-none text-[30px] bg-transparent text-white caret-transparent placeholder-transparent
+`;
+const Button = tw.button`h-12 text-2xl px-3 font-medium text-white bg-[#d789893d] border rounded-md border-white focus:outline-none focus:bg-[#d789895c]
+`;
+
+const Otp = () => {
+	const [enteredOtp, setEnteredOtp] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	const handleInput = (e) => {
+		if (e.target.value.length > 4 && enteredOtp.length === 4) return;
+		setEnteredOtp(e.target.value);
+	};
+
+	return (
+		<Wrapper onClick={handleSubmit}>
+			<SubHeading>Enter OTP</SubHeading>
+			<div className='relative'>
+				<div className='absolute z-[-80] left-[calc(50%+45px)] top-1 w-10 h-10 border-solid border-white border-2'></div>
+				<div className='absolute z-[-80] left-[calc(50%-2px)] top-1 w-10 h-10 border-solid border-white border-2'></div>
+				<div className='absolute z-[-80] left-[calc(50%-49px)] top-1 w-10 h-10 border-solid border-white border-2'></div>
+				<div className='absolute z-[-80] left-[calc(50%-95px)] top-1 w-10 h-10 border-solid border-white border-2'></div>
+
+				<Input
+					value={enteredOtp}
+					onChange={handleInput}
+					placeholder='Enter OTP'
+				/>
+			</div>
+			<Button>Submit</Button>
+		</Wrapper>
+	);
+};
+
+export default Otp;
---
+++ src/pages/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import tw from 'tailwind-styled-components';
+import background from '../assets/background.svg';
+import Otp from '../components/Otp';
+
+const Container = tw.div`w-full h-full bg-cover bg-center flex items-center justify-center
+`;
+const Background = tw.img`w-full h-full absolute top-0 left-0 bottom-0 right-0 object-cover object-center z-[-999]
+`;
+const Wrapper = tw.form`md:min-h-[80vh] min-h-[60vh] w-full max-w-[90vw] bg-gray-100 bg-opacity-40 rounded-3xl shadow-2xl flex flex-col items-center justify-center py-8 space-y-9
+`;
+const Heading = tw.h1`text-5xl font-medium md:text-8xl md:font-normal text-center text-white
+`;
+const SubHeading = tw.h2`text-3xl font-medium md:text-5xl md:font-normal text-center text-white
+`;
+const Input = tw.input`md:w-96 w-72 max-w-[90%] text-2xl h-12 pl-20 border-b border-white focus:outline-none md:text-3xl bg-transparent text-white
+`;
+const SendButton = tw.button`h-12 text-2xl px-3 font-medium text-white bg-[#d789893d] border rounded-md border-white focus:outline-none focus:bg-[#d789895c]
+`;
+
+const Login = () => {
+	const [enteringOTP, setEnteringOTP] = useState(false);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setEnteringOTP(true);
+	};
+
+	return (
+		<Container>
+			<Background src={background} />
+			{!enteringOTP ? (
+				<Wrapper onClick={handleSubmit}>
+					<Heading>Login</Heading>
+					<SubHeading>Enter Mobile No.</SubHeading>
+					<div className='relative text-center'>
+						<label
+							htmlFor='mobile'
+							className='absolute inset-y-0 left-4 text-white text-2xl md:text-3xl flex items-center pl-3'
+						>
+							+91
+						</label>
+						<Input type='text' placeholder='Mobile No.' id='mobile' />
+					</div>
+					<SendButton type='submit'>Send OTP</SendButton>
+				</Wrapper>
+			) : (
+				<Otp />
+			)}
+		</Container>
+	);
+};
+
+export default Login;
